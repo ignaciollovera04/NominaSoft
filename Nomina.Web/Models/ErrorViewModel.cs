b608cc9ed2c8997ef9c6c25dd63d4cdc112879d0
ignaciollovera04 <ignaciollovera04@gmail.com>
2025-10-16 16:52:18 -0500

    Ignorar carpeta .vs

---
+++ Nomina.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nomina.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
